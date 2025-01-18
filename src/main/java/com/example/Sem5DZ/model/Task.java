2392d6a42c96ce0182c9821f781124f676de4f8d
Evgeniy K <kovalev8915512@gmail.com>
2025-01-18 13:55:57 +0300

    create model

--- src/main/java/com/example/Sem5DZ/model/Task.java
+++ src/main/java/com/example/Sem5DZ/model/Task.java
@@ -1,10 +1,12 @@
 package com.example.Sem5DZ.model;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
@@ -15,9 +17,16 @@ public class Task {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  Long id;
 
+    @Column( nullable=false)
     private String description;
 
+
+
+    @Column( nullable=false)
     private TaskStatus status;
 
-    private LocalDateTime dateTime;
+
+    @Column( nullable=false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
---
+++ src/main/java/com/example/Sem5DZ/model/TaskStatus.java
@@ -0,0 +1,6 @@
+package com.example.Sem5DZ.model;
+
+public enum TaskStatus {
+
+    NOT_STARTED, IN_PROGRESS,COMPLETED;
+}
